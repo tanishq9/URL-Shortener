2dab359fb24772adc7418e3e3e6b03c2a55eb1fb
Tanishq Saluja <tanishqsaluja18@gmail.com>
2019-02-07 22:51:02 +0530

    feat : added copy to clipboard using plain js

--- public/script.js
+++ public/script.js
@@ -1,15 +1,34 @@
 $(function(){
     let shorten_button = $('#shorten_button');
     let shorten_text = $('#shorten_text');
-    shorten_button.click(function(){
+    let copy_div = $('#copy_div');
+    let copy_text = $('#copy_text');
+    let copy_button = $('#copy_button');
+    let copy_success_message = $('#copy_message');
+    let text_to_be_copied = $('#shortentextlink');
+    shorten_button.click(function(){   
         console.log('Button clicked.');
         $.post(
             '/shorten',
             {url : shorten_text.val()}, // this will be send to /shorten
             function(data){ //  response of post request
-                console.log(data);
-                window.prompt("Copy to clipboard: Ctrl+C, Enter", 'https://g-z.herokuapp.com/'+data.hash);
+                copy_div.show(); // display clipboard and success message
+                console.log(data.hash);
+                copy_text.text('https://g-z.herokuapp.com/'+data.hash);
+                text_to_be_copied.text('https://g-z.herokuapp.com/'+data.hash);
+                //window.prompt("Copy to clipboard: Ctrl+C, Enter", 'https://g-z.herokuapp.com/'+data.hash);
             }
         )
     })
+
+    copy_button.click(function(){
+         // clipboard logic
+        copy_success_message.show();
+        text_to_be_copied.select();
+        document.execCommand("copy");
+        setTimeout(function(){
+           copy_success_message.hide();
+        },1500);
+     })
+
 });
